0e4b4a3774cce8da8c8f100fa5e926d09534b849
erikfenriz <erikfenriz@gmail.com>
2018-11-30 10:47:52 +0200

    main page mark up animations and transtions added. Code was refactored

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,29 +5,27 @@ import {Link} from 'react-router-dom';
 
 export const Main = () => (
     <main className="main-page">
-        <section className="main-page__nesting">
-            <section className="experience">
-                <div className="experience__title">
-                    <div className="experience__title--container-headline">
-                        <h1 className="experience__title--headline">EXPERIENCES by MANE</h1>
+        <section className="main-page__content">
+            <section className="intro">
+                <div className="intro__title">
+                    <div className="intro__title--container-headline">
+                        <h1 className="intro__title--headline">EXPERIENCES by MANE</h1>
                     </div>
-                    <h2 className="experience__title--secondary">Dedicated to perfume lovers</h2>
-                    <ul className="experience__title--list">
+                    <h2 className="intro__title--secondary">Dedicated to perfume lovers</h2>
+                    <ul className="intro__title--list">
                         <li>What’s your mood today?</li>
                         <li>What’s your perfect perfume?</li>
                         <li>What’s your “it” colour?</li>
                         <li>What’s your inspiring rhythm?</li>
                     </ul>
-                    <p className="experience__title--summary">EXPERIENCES by MANE help you find the answers to the above
-
-
+                    <p className="intro__title--summary">EXPERIENCES by MANE help you find the answers to the above
                         to many other questions on the magic world of perfume.
                     </p>
                 </div>
 
             </section>
-            <div id="asd">
-                <div id="test">
+            <div className="experience">
+                <div className="experience__choice">
                     <Link to="/LetPerfumeTalk" className="experience__perfume">
                         <div className="experience__link--container">
                             <h3 className="experience__link--title">Let perfume talk</h3>

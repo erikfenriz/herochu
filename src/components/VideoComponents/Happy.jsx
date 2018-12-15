1c9f6cc70685358aa6423f9418c171506acd95da
erikfenriz <erikfenriz@gmail.com>
2018-12-15 18:34:58 +0200

    ready for deployment (be cautious while merging)

--- src/components/VideoComponents/Happy.jsx
+++ src/components/VideoComponents/Happy.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 import happyMP4 from '../../assets/videos/experience2/mp4/happy.mp4';
 import happyWebM from "../../assets/videos/experience2/webm/happy.webm";
-import $ from "jquery";
 // import happyMP3 from "../../assets/audio/happy.mp3";
+import featherClick from "../../assets/images/experience2/cursor/feather-click.png";
+import featherClicked from "../../assets/images/experience2/cursor/feather-clicked.png";
+import {rotatingCursor} from "./cursor";
+// import {TweenMax} from "gsap/TweenMax";
+// import $ from 'jquery';
+import pineapple from '../../assets/images/experience2/elements/happy/gr_ananas.png';
+import sun from '../../assets/images/experience2/elements/happy/gr_sun.png';
+// import candy1 from '../../assets/images/experience2/elements/happy/gr_candy1.png';
+// import candy2 from '../../assets/images/experience2/elements/happy/gr_candy2.png';
+// import iceCream from '../../assets/images/experience2/elements/happy/gr_icecream.png';
+// import note from '../../assets/images/experience2/elements/happy/gr_musicnote.png';
 
 export default class Happy extends Component {
     setTitle = () => {
@@ -13,8 +22,14 @@ export default class Happy extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            mute: false,
+            strikeThrough: "menu__bottom--happy",
+            displayCursor: true,
+            cursor: "cursor__click",
             x: Number,
-            y: Number
+            y: Number,
+            element: [],
+            loader: "loading"
         };
         this.video = React.createRef();
         // this.audio = React.createRef();
@@ -22,93 +37,177 @@ export default class Happy extends Component {
         this.setCoordinates = this.setCoordinates.bind(this);
     };
 
-    // stile = {
-    //     top: this.state.x + "px",
-    //     left: this.state.y + "px",
-    //     position: 'absolute'
-    // };
-
     setCoordinates = e => {
         this.setState({x: e.pageX, y: e.pageY});
+        if (this.video.current)
+            if ((e.buttons === 1 || e.which === 1) && this.state.displayCursor === true) {
+                if (this.video.current.currentTime < 5) {
+                    let element = React.createElement('img', {
+                        src: sun,
+                        alt: "experience",
+                        key: Math.random(),
+                        style: {
+                            top: this.state.y - 80 + "px",
+                            left: this.state.x - 50 + "px",
+                            position: 'fixed',
+                            animation: 'anima ' + 3 + 's',
+                            opacity: 0,
+                        }
+                    });
+                    this.setState(prevState => ({
+                        element: [...prevState.element, element]
+                    }));
+                } else if (this.video.current.currentTime > 5 && this.video.current.currentTime < 10) {
+                    let element = React.createElement('img', {
+                        src: pineapple,
+                        alt: "experience",
+                        key: Math.random(),
+                        style: {
+                            top: this.state.y - 80 + "px",
+                            left: this.state.x - 50 + "px",
+                            position: 'fixed',
+                            animation: 'anima ' + 3 + 's',
+                            opacity: 0,
+                        }
+                    });
+                    this.setState(prevState => ({
+                        element: [...prevState.element, element]
+                    }));
+                }
+            }
     };
 
     playVideo = () => {
         this.video.current.play();
-        $('.letPerfumeTalk__video').get(0).play();
     };
 
-    videoStarts = () => {
-        // this.video.current.muted = false;
-        // this.audio.current.play();
+    hide = () => {
+        const cursor = document.getElementById('cursor');
+        cursor.style.display = 'none';
+        this.setState({displayCursor: false});
     };
 
-    setMouse = (e) => {
-        // $('.test').css({
-        //     left:  e.pageX + 30 + "px",
-        //     top:   e.pageY
-        // });
+    display = () => {
+        const cursor = document.getElementById('cursor');
+        cursor.style.display = 'block';
+        this.setState({displayCursor: true});
+    };
 
-        // ReactDOM.findDOMNode(this.refs.test).style = {
-        //     left:  e.pageX + 30 + "px",
-        //     top:   e.pageY,
-        //     position: 'absolute'
-        // };
-        // $('.test').css({
-        //     left:  e.pageX + 30 + "px",
-        //     top:   e.pageY
-        // });
-        // console.log(ReactDOM.findDOMNode(this.refs.test).style.left);
+    mute = () => {
+        if (this.state.strikeThrough === "menu__bottom--happy") {
+            this.setState({strikeThrough: "menu__bottom--happy strikethrough"});
+        } else {
+            this.setState({strikeThrough: "menu__bottom--happy"});
+        }
+        // if (this.state.strikeThrough === "menu__bottom--happy strikethrough") {
+        //     this.video.current.muted = true;
+        // } else{
+        //     this.video.current.muted = false;
+        // }
+        this.state.strikeThrough === "menu__bottom--happy strikethrough"
+            ? this.video.current.muted = false :
+            this.video.current.muted = true;
+
+        console.log(this.video.current.muted);
     };
 
     clicked = () => {
         this.video.current.play();
-        this.video.current.muted = false;
-        // this.refs.test.style.opacity = 0;
-        do {
-            this.refs.test.style.opacity--
-        } while (this.refs.test.style.opacity > 0);
+        // this.video.current.muted = false;
+        this.setState({
+            cursor: "cursor__click--clicked"
+        });
+    };
 
-        if (this.refs.test.style.opacity === 0) {
-            this.refs.test.remove();
-        }
+    loaded = () => {
+        this.setState({loader: "loading loaded"});
+        this.video.current.play();
     };
 
     componentDidMount() {
         this.setTitle();
-        this.playVideo();
-        setTimeout(this.videoStarts, 300);
+
         document.addEventListener('contextmenu', event => event.preventDefault());
+        rotatingCursor.initialize();
+        this.playVideo();
+        this.setCoordinates.bind(this);
+        document.addEventListener('click', this.setCoordinates);
         document.addEventListener('mousemove', this.setCoordinates);
+        setTimeout(this.loaded, 1000);
+        // document.addEventListener('DOMContentLoaded', this.loaded);
     };
 
 
     render() {
         return (
-            <main onClick={this.clicked} className="letPerfumeTalk"
-                  onMouseMove={this.setCoordinates.bind(this)}>
-                <video className="letPerfumeTalk__video"
-                       autoPlay={true}
-                       loop={true}
-                       muted={true}
-                       ref={this.video}
+            <React.Fragment>
+                <main onClick={this.clicked}
+                      className="letPerfumeTalk"
                 >
-                    <source src={happyMP4} type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'/>
-                    <source src={happyWebM} type="video/webm"/>
-                </video>
-                {/*<audio autoPlay*/}
-                {/*ref={this.audio}>*/}
-                {/*<source src={happyMP3} type="audio/mpeg"/>*/}
-                {/*</audio>*/}
-                <div className="test"
-                     ref='test'
-                     style={{
-                         top: this.state.y + 0 + "px",
-                         left: this.state.x + 30 + "px",
-                     }}>
-                    Click, drag<br/>
-                    & enjoy!
+                    <video className="letPerfumeTalk__video"
+                           autoPlay={true}
+                           loop={true}
+                           muted={false}
+                           ref={this.video}
+                    >
+                        <source src={happyMP4} type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'/>
+                        <source src={happyWebM} type="video/webm"/>
+                    </video>
+                    {/*<audio autoPlay*/}
+                    {/*ref={this.audio}>*/}
+                    {/*<source src={happyMP3} type="audio/mpeg"/>*/}
+                    {/*</audio>*/}
+                    {this.state.element}
+                    <div ref="cursor" id="cursor">
+                        <img className={this.state.cursor} alt="feather" src={featherClick}/>
+                        <img className="cursor__clicked" alt="feather" src={featherClicked}/>
+                    </div>
+
+                    <div className="menu">
+                        <div className="menu__top">
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 className="menu__top--button menu__circle--top menu__circle--happy">
+                                <p>Back</p>
+                            </div>
+                        </div>
+
+                        <div className="menu__bottom">
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 className="menu__bottom--button menu__bottom--side">
+                                <p className="menu__bottom--happy">Share</p>
+                            </div>
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 className="menu__bottom--button menu__circle--bottom menu__circle--happy">
+                                <p>Happy</p>
+                            </div>
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 onClick={this.mute}
+                                 className="menu__bottom--button menu__bottom--side">
+                                <p className={this.state.strikeThrough}>Sound</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="LetPerfumeTalkByMane">
+                        <p className="LetPerfumeTalkByMane__experienceName">Let Perfume Talk
+                            <span className="LetPerfumeTalkByMane__experienceName--small">&nbsp;by MANE</span>
+                        </p>
+                    </div>
+                </main>
+                <div className={this.state.loader}>
+                    <div className="loading__text">
+                        <h1>Loading</h1>
+                        <div className="three-bounce">
+                            <div className="one"></div>
+                            <div className="two"></div>
+                            <div className="three"></div>
+                        </div>
+                    </div>
                 </div>
-            </main>
+            </React.Fragment>
         )
     }
 }
--- src/components/VideoComponents/Natural.jsx
+++ src/components/VideoComponents/Natural.jsx
@@ -1,55 +1,213 @@
 import React, {Component} from 'react';
-import naturalMP4 from '../../assets/videos/experience2/mp4/natural.mp4';
-import naturalWebM from "../../assets/videos/experience2/webm/natural.webm";
-import $ from "jquery";
+import happyMP4 from '../../assets/videos/experience2/mp4/natural.mp4';
+import happyWebM from "../../assets/videos/experience2/webm/natural.webm";
+// import happyMP3 from "../../assets/audio/happy.mp3";
+import featherClick from "../../assets/images/experience2/cursor/feather-click.png";
+import featherClicked from "../../assets/images/experience2/cursor/feather-clicked.png";
+import {rotatingCursor} from "./cursor";
+// import {TweenMax} from "gsap/TweenMax";
+// import $ from 'jquery';
+import pineapple from '../../assets/images/experience2/elements/happy/gr_ananas.png';
+import sun from '../../assets/images/experience2/elements/happy/gr_sun.png';
+// import candy1 from '../../assets/images/experience2/elements/happy/gr_candy1.png';
+// import candy2 from '../../assets/images/experience2/elements/happy/gr_candy2.png';
+// import iceCream from '../../assets/images/experience2/elements/happy/gr_icecream.png';
+// import note from '../../assets/images/experience2/elements/happy/gr_musicnote.png';
 
-export default class Natural extends Component {
+export default class Happy extends Component {
     setTitle = () => {
         document.title = "Feeling natural today? Let Perfume Talk by MANE";
     };
 
     constructor(props) {
         super(props);
-        this.state = { x: 0, y: 0 };
+        this.state = {
+            mute: false,
+            strikeThrough: "menu__bottom--happy",
+            displayCursor: true,
+            cursor: "cursor__click",
+            x: Number,
+            y: Number,
+            element: [],
+            loader: "loading"
+        };
         this.video = React.createRef();
+        // this.audio = React.createRef();
+        this.test = React.createRef();
+        this.setCoordinates = this.setCoordinates.bind(this);
     };
 
-    onMouseMove(e) {
-        this.setState({ x: e.screenX, y: e.screenY });
+    setCoordinates = e => {
+        this.setState({x: e.pageX, y: e.pageY});
+        if (this.video.current)
+            if ((e.buttons === 1 || e.which === 1) && this.state.displayCursor === true) {
+                if (this.video.current.currentTime < 5) {
+                    let element = React.createElement('img', {
+                        src: sun,
+                        alt: "experience",
+                        key: Math.random(),
+                        style: {
+                            top: this.state.y - 80 + "px",
+                            left: this.state.x - 50 + "px",
+                            position: 'fixed',
+                            animation: 'anima ' + 3 + 's',
+                            opacity: 0,
+                        }
+                    });
+                    this.setState(prevState => ({
+                        element: [...prevState.element, element]
+                    }));
+                } else if (this.video.current.currentTime > 5 && this.video.current.currentTime < 10) {
+                    let element = React.createElement('img', {
+                        src: pineapple,
+                        alt: "experience",
+                        key: Math.random(),
+                        style: {
+                            top: this.state.y - 80 + "px",
+                            left: this.state.x - 50 + "px",
+                            position: 'fixed',
+                            animation: 'anima ' + 3 + 's',
+                            opacity: 0,
+                        }
+                    });
+                    this.setState(prevState => ({
+                        element: [...prevState.element, element]
+                    }));
+                }
+            }
     };
 
     playVideo = () => {
         this.video.current.play();
-        $('.letPerfumeTalk__video').get(0).play();
     };
 
-    videoStarts = () => {
-        this.video.current.muted = false;
+    hide = () => {
+        const cursor = document.getElementById('cursor');
+        cursor.style.display = 'none';
+        this.setState({displayCursor: false});
+    };
+
+    display = () => {
+        const cursor = document.getElementById('cursor');
+        cursor.style.display = 'block';
+        this.setState({displayCursor: true});
+    };
+
+    mute = () => {
+        if (this.state.strikeThrough === "menu__bottom--happy") {
+            this.setState({strikeThrough: "menu__bottom--happy strikethrough"});
+        } else {
+            this.setState({strikeThrough: "menu__bottom--happy"});
+        }
+        // if (this.state.strikeThrough === "menu__bottom--happy strikethrough") {
+        //     this.video.current.muted = true;
+        // } else{
+        //     this.video.current.muted = false;
+        // }
+        this.state.strikeThrough === "menu__bottom--happy strikethrough"
+            ? this.video.current.muted = false :
+            this.video.current.muted = true;
+
+        console.log(this.video.current.muted);
+    };
+
+    clicked = () => {
+        this.video.current.play();
+        // this.video.current.muted = false;
+        this.setState({
+            cursor: "cursor__click--clicked"
+        });
+    };
+
+    loaded = () => {
+        this.setState({loader: "loading loaded"});
+        this.video.current.play();
     };
 
     componentDidMount() {
         this.setTitle();
+
+        document.addEventListener('contextmenu', event => event.preventDefault());
+        rotatingCursor.initialize();
         this.playVideo();
-        setTimeout(this.videoStarts,300);
+        this.setCoordinates.bind(this);
+        document.addEventListener('click', this.setCoordinates);
+        document.addEventListener('mousemove', this.setCoordinates);
+        setTimeout(this.loaded, 1000);
+        // document.addEventListener('DOMContentLoaded', this.loaded);
     };
 
-    clicked = () =>{
-        this.video.current.play();
-    };
 
     render() {
         return (
-            <main onClick={this.clicked} className="letPerfumeTalk" onMouseMove={this.onMouseMove.bind(this)}>
-                <video className="letPerfumeTalk__video"
-                       autoPlay={true}
-                       loop={true}
-                       muted={false}
-                       ref={this.video}
+            <React.Fragment>
+                <main onClick={this.clicked}
+                      className="letPerfumeTalk"
                 >
-                    <source src={naturalMP4} type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'/>
-                    <source src={naturalWebM} type="video/webm"/>
-                </video>
-            </main>
+                    <video className="letPerfumeTalk__video"
+                           autoPlay={true}
+                           loop={true}
+                           muted={false}
+                           ref={this.video}
+                    >
+                        <source src={happyMP4} type='video/mp4; codecs="avc1.4D401E, mp4a.40.2"'/>
+                        <source src={happyWebM} type="video/webm"/>
+                    </video>
+                    {/*<audio autoPlay*/}
+                    {/*ref={this.audio}>*/}
+                    {/*<source src={happyMP3} type="audio/mpeg"/>*/}
+                    {/*</audio>*/}
+                    {this.state.element}
+                    <div ref="cursor" id="cursor">
+                        <img className={this.state.cursor} alt="feather" src={featherClick}/>
+                        <img className="cursor__clicked" alt="feather" src={featherClicked}/>
+                    </div>
+
+                    <div className="menu">
+                        <div className="menu__top">
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 className="menu__top--button menu__circle--top menu__circle--happy">
+                                <p>Back</p>
+                            </div>
+                        </div>
+
+                        <div className="menu__bottom">
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 className="menu__bottom--button menu__bottom--side">
+                                <p className="menu__bottom--happy">Share</p>
+                            </div>
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 className="menu__bottom--button menu__circle--bottom menu__circle--happy">
+                                <p>Happy</p>
+                            </div>
+                            <div onMouseOver={this.hide}
+                                 onMouseOut={this.display}
+                                 onClick={this.mute}
+                                 className="menu__bottom--button menu__bottom--side">
+                                <p className={this.state.strikeThrough}>Sound</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="LetPerfumeTalkByMane">
+                        <p className="LetPerfumeTalkByMane__experienceName">Let Perfume Talk
+                            <span className="LetPerfumeTalkByMane__experienceName--small">&nbsp;by MANE</span>
+                        </p>
+                    </div>
+                </main>
+                <div className={this.state.loader}>
+                    <div className="loading__text">
+                        <h1>Loading</h1>
+                        <div className="three-bounce">
+                            <div className="one"></div>
+                            <div className="two"></div>
+                            <div className="three"></div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }

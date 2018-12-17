daa77fda611ceafe9cde591f6ed07bd09d8ace32
Pasha <pgutzu@gmail.com>
2018-12-17 10:30:30 +0300

    comment carousel and add if()

--- src/components/SmellMenu/components/BottleItem.jsx
+++ src/components/SmellMenu/components/BottleItem.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import { compose, withState, withHandlers } from 'recompose';
 
 const BottleItem = props => {
   const { visibleChangeButtonFalse, visibleChangeButton, visible, count } = props;
 
-  const { name, color, img, imgGif, colorNext } = props.item;
+  const { name, color, img, imgGif, colorNext, link } = props.item;
   const gradient = "linear-gradient(to bottom right," + color + ", " + colorNext + ")";
   const countItem = "item" + count;
   // console.log(props);
   return (
     // <figure id={countItem} class="carouselItem trans3d">
-   
-      <div
-        // id={countItem}
-        
-        key={name}
-        // onMouseOver={visibleChangeButton}
-        // onMouseLeave={visibleChangeButtonFalse}
-        className="bottle_item"
-        style={{width:"230px"}}
-      >
-        <center>
-          
+
+    <div
+      // id={countItem}
+
+      key={name}
+      onMouseOver={visibleChangeButton}
+      onMouseLeave={visibleChangeButtonFalse}
+      className="bottle_item"
+      style={{ width: "230px" }}
+    >
+      <center>
+        <Link to={link}>
           <div style={{ opacity: visible, background: gradient }} className="play_circle">
             <p className="play_circle_text">play</p>
           </div>
-          {visible === 0 ? <img  className={countItem} alt={name} src={img} /> : <img  className={countItem} alt={name} src={imgGif} />}
-        </center>
-      </div>
+        </Link>
+        {visible === 0 ? <img className={countItem} alt={name} src={img} /> : <img className={countItem} alt={name} src={imgGif} />}
+      </center>
+    </div>
     // </figure>
   );
 }

eef85e8cd32865acb12fc8a862e27c50608148a5
erikfenriz <erikfenriz@gmail.com>
2019-01-08 14:40:40 +0200

    fade in and out

--- src/components/SmellMenu/components/test.jsx
+++ src/components/SmellMenu/components/test.jsx
@@ -3,11 +3,14 @@ export default class test extends Component {
   
   render() {
 
-    const {  visible } = this.props;
+    const {  visible ,name} = this.props;
+    console.log(name);
     return (<div>
-      {visible ?
-          <div>
-            <div className='smellMenu__svg' id="pasha">
+        <center>
+            <div style={{ color: this.props.colorNext }} className='smellMenu__text__name'>{name}</div>
+          </center> 
+          <div className='smellMenu__all'>
+            <div className='smellMenu__svg'>
               <svg width="400" height="40" xmlns="http://www.w3.org/2000/svg">
                 <line x1="340" y1="20" x2="180" y2="20" stroke="green" strokeWidth="1" />
                 <polygon fill="green" points="250 20, 270 20, 260 30" />
@@ -15,11 +18,7 @@ export default class test extends Component {
             </div>
             <div style={{ opacity: 1 }} className='smellMenu__text__perfume'>Choose your perfume according to your instant emotion.</div>
           </div>
-           :
-          <center>
-            <div style={{ color: this.props.colorNext }} className='smellMenu__text__name'>{ this.props.name}</div>
-          </center>
-        } 
+        
      </div>
     );
   }
